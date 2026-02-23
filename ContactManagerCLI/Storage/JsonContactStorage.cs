28e7e075f805ddfd442691804eef6f1159980597
DimaKhaled <dkh3462@gmail.com>
2026-02-23 23:16:44 +0200

    Implement JSON storage for contacts

--- ContactManagerCLI/Storage/JsonContactStorage.cs
+++ ContactManagerCLI/Storage/JsonContactStorage.cs
@@ -1,12 +1,55 @@
 ﻿using ContactManagerCLI.Models;
-using System.IO;
+using ContactManagerCLI.Storage;
 using System.Collections.Generic;
+using System.IO;
 using System.Text.Json;
 
 namespace ContactManagerCLI.Storage
 {
-    internal class JsonContactStorage
+    internal class JsonContactStorage : IContactStorage
     {
-        
+        private readonly string _filePath;
+
+
+        public JsonContactStorage(string filePath)
+        {
+            _filePath = filePath;
+        }
+
+
+        public List<Contact> LoadContacts()
+        {
+            try
+            {
+                if (!File.Exists(_filePath))
+                    return new List<Contact>();
+
+                string json = File.ReadAllText(_filePath);
+
+                if (string.IsNullOrWhiteSpace(json))
+                    return new List<Contact>();
+
+                return JsonSerializer.Deserialize<List<Contact>>(json) ?? new List<Contact>();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error happened while loading contacts: " + e.Message);
+                return new List<Contact>();
+            }
+        }
+
+
+        public void SaveContacts(List<Contact> contacts)
+        {
+            try
+            {
+                string json = JsonSerializer.Serialize(contacts, new JsonSerializerOptions { WriteIndented = true });
+                File.WriteAllText(_filePath, json);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error happened while saving contacts: " + e.Message);
+            }
+        }
     }
 }
