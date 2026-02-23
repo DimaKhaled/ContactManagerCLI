583c6edd4223d50de16f4f7ab4b41503498e066b
DimaKhaled <dkh3462@gmail.com>
2026-02-23 20:14:38 +0200

    Add Contact model with required properties

--- ContactManagerCLI/Models/Contact.cs
+++ ContactManagerCLI/Models/Contact.cs
@@ -6,6 +6,10 @@ namespace ContactManagerCLI.Models
 {
     internal class Contact
     {
-        
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public DateTime CreationDate { get; set; }
     }
 }
