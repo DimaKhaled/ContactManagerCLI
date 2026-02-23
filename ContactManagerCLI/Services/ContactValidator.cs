def2d5fb61d75c1e7272739aa17ff876cf1143a5
DimaKhaled <dkh3462@gmail.com>
2026-02-24 00:53:24 +0200

    Implement validation logic.

--- ContactManagerCLI/Services/ContactValidator.cs
+++ ContactManagerCLI/Services/ContactValidator.cs
@@ -1,10 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace ContactManagerCLI.Services
 {
-    internal class ContactValidator
+    internal class ContactValidator : IInputValidator
     {
+        public bool isValidName(string name)
+        {
+            return !string.IsNullOrWhiteSpace(name);
+        }
+
+
+        public bool isValidPhone(string phone)
+        {
+            return !string.IsNullOrWhiteSpace(phone) && phone.All(char.IsDigit) && phone.Length == 11;
+        }
+
+
+        public bool isValidEmail(string email)
+        {
+            if (string.IsNullOrWhiteSpace(email))
+                return false;
+            
+            return Regex.IsMatch(email, @"^[^@\s]+@[^@\s]+\.[^@\s]+$");
+        }
     }
 }
